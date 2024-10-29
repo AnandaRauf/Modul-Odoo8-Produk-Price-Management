fac1faf011483f33cbbdaf7206760b6329c77414
Ananda Rauf <anandarauf02@gmail.com>
2024-10-29 13:59:12 +0700

    Update __openerp__.py

--- produk_price/__openerp__.py
+++ produk_price/__openerp__.py
@@ -5,11 +5,12 @@
     'category': 'Product',
     'description': 'Module to manage products with image, name, description, and price.',
     'author': 'Ananda Rauf Maududi',
-    'depends': ['base'],
+    'depends': ['base','sale'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/product.xml',
+        'views/sale_order_inherit_view.xml',
     ],
     'installable': True,
     'auto_install': False,
