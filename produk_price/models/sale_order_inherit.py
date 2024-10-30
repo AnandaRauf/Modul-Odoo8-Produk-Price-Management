69891c8e8d30c2c14761681404516e637a628174
Ananda Rauf <anandarauf02@gmail.com>
2024-10-30 11:32:22 +0700

    Update sale_order_inherit.py

--- produk_price/models/sale_order_inherit.py
+++ produk_price/models/sale_order_inherit.py
@@ -1,7 +1,7 @@
 from openerp import api, models, fields
-from openerp.exceptions import AccessError
+from openerp.exceptions import MissingError
 
-class SaleOrderDates(models.Model):  
+class SaleOrderLine(models.Model):  
     _inherit = 'sale.order.line'
 
     product_id = fields.Many2one(
@@ -11,20 +11,31 @@ class SaleOrderDates(models.Model):
     
     price_unit = fields.Float(
         string='Price Unit',
-        related='product_id.price',  # Mengambil harga dari `product.custom`
+        related='product_id.price',  # Pulls price from `product.custom`
         store=True,
         readonly=True
     )
 
-    quantity = fields.Integer('Quantity', required=True, default=1)
-    
-    total_price = fields.Float(
-        string='Total Price',
-        compute='_compute_total_price',
-        store=True
-    )
+    @api.onchange('product_id', 'price_unit')
+    def _onchange_product_id(self):
+        if not self.product_id:
+            self.price_unit = 0
+        else:
+            try:
+                self.price_unit = self.product_id.price
+            except MissingError:
+                self.product_id = False
+                self.price_unit = 0
 
-    @api.depends('price_unit', 'quantity')
-    def _compute_total_price(self):
-        for record in self:
-            record.total_price = record.price_unit * record.quantity
+    
+    # quantity = fields.Integer('Quantity', required=True, default=1)
+    # total_price = fields.Float(
+    #     string='Total Price',
+    #     compute='_compute_total_price',
+    #     store=True
+    # )
+    #
+    # @api.depends('price_unit', 'quantity')
+    # def _compute_total_price(self):
+    #     for record in self:
+    #         record.total_price = record.price_unit * record.quantity
